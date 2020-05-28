bbf555f625968e4e26e7fe8b97a0a497d36cb7a3
jinyand <jiiin97@naver.com>
2020-05-28 17:00:05 +0900

    jinyand - main page 중간부분

--- app/src/main/java/com/tistory/comfy91/clientanddesignseminar/MainActivity.kt
+++ app/src/main/java/com/tistory/comfy91/clientanddesignseminar/MainActivity.kt
@@ -2,11 +2,19 @@ package com.tistory.comfy91.clientanddesignseminar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_viewPager.adapter = TabPagerAdapter(supportFragmentManager)
+        main_viewPager.offscreenPageLimit = 1
+
+        main_tab.setupWithViewPager(main_viewPager)
+        main_tab.getTabAt(0)!!.setText("즐겨찾기")
+        main_tab.getTabAt(1)!!.setText("전체 카테고리")
     }
 }
