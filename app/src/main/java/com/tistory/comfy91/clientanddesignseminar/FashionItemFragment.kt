632645bb9a010c222f08c56602a0ada9f22ce05d
KimSuhyeon <kshvv975@gmail.com>
2020-05-30 15:10:28 +0900

    탭 레이아웃 아이탬 글꼴 적용 제외하고 다 완료

--- app/src/main/java/com/tistory/comfy91/clientanddesignseminar/FashionItemFragment.kt
+++ app/src/main/java/com/tistory/comfy91/clientanddesignseminar/FashionItemFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.fragment_fashion_item.*
 
 
 class FashionItemFragment : Fragment() {
@@ -14,8 +14,21 @@ class FashionItemFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fashion_item, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        tv_popular_keyword1.text = "# 시계"
+        tv_popular_keyword2.text = "# 구찌"
+        tv_popular_keyword3.text = "# 코치"
+        tv_popular_keyword4.text = "# 메트로시티"
+        tv_popular_keyword5.text = "# 고야드"
+        tv_popular_keyword6.text = "# 290"
+        tv_popular_keyword7.text = "# 제이에스티나"
+        tv_popular_keyword8.text = "# 가방"
+        tv_popular_keyword9.text = "# 지갑"
+        tv_popular_keyword10.text = "# 프라다"
     }
 
     companion object {
